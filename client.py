7d07907b93941373358cd38ac211b9652e5a826e
ISRAVEL Y <isravelyosva@gmail.com>
2026-05-11 09:08:22 +0530

    Add sliding window protocol client implementation

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+
+client = socket.socket()
+client.connect(('localhost', 8000))
+
+n = int(input("Enter number of frames: "))
+w = int(input("Enter window size: "))
+
+frames = list(range(1, n + 1))
+
+i = 0
+
+while i < n:
+    send_frames = frames[i:i + w]
+    msg = " ".join(map(str, send_frames))
+
+    print("Sending frames:", msg)
+    client.send(msg.encode())
+
+    ack = client.recv(1024).decode()
+    print("Received:", ack)
+
+    i += w
+
+client.close()
